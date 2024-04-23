f738038abd067beb350adcaebd79342e26d32a47
Amirho3einxD <167878868+Amirho3einxD@users.noreply.github.com>
2024-04-23 16:03:43 +0330

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,17 @@
+a = int(input("Number1:"))
+b = int(input("Number2:"))
+c = int(input("Number3:"))
+d = int(input("Number4:"))
+
+if a <= b :
+    x=a
+else:
+    x=b
+if c<=d : y=c
+else: y=d
+
+if x<=y:print(x)
+else: print(y)
+
+      
+
